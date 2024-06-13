4c9aeca24b87e262a8de1e5b84544ebf75962d3f
logate-technologies <support@logate.co.il>
2024-06-13 12:56:40 +0300

    Added logic To login.aspx.cs

--- Ecommerce/Login.aspx.cs
+++ Ecommerce/Login.aspx.cs
@@ -11,7 +11,10 @@ namespace Ecommerce
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+           if(!IsPostBack)
+            {
+
+            }
         }
 
       
